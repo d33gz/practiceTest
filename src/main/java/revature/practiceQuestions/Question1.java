32a32295b58e537c5f936810251b6ddc6745ab68
Hendo1349 <hendo1349@gmail.com>
2022-03-22 09:38:27 -0400

    add questions

--- src/main/java/revature/practiceQuestions/Question1.java
+++ src/main/java/revature/practiceQuestions/Question1.java
@@ -1,10 +1,12 @@
 package revature.practiceQuestions;
 
 public class Question1 {
+	//Given an int and a int Array, find out how many ints in the Array are Greater than the given int.
 	public static int countGreater(int x, int[] y) {
 		int result = 0;
 		//Start your Code here
 
 		//End your Code here
+		return result;
 	}
 }
